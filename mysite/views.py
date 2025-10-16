5e240964297da19a26570f4e11d0e9e5890ed080
Joemer <joemers97@gmail.com>
2025-10-16 17:34:46 -0400

    Link Django view to base HTML template

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-  return HttpResponse('<h3>This is our homepage</h3>')
+  return render(request, 'home.html')
